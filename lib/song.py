82a7b8410a5b38fe08e26b74cf98d07202d25a28
Okaroni Purity <nekesapurity20@gmail.com>
2025-08-17 14:09:47 +0300

    completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,58 @@
+# lib/classes/song.py
 class Song:
-    pass
+    all = []
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        if not isinstance(name, str) or not name:
+            raise ValueError("Name must be a non-empty string")
+        if not isinstance(artist, str) or not artist:
+            raise ValueError("Artist must be a non-empty string")
+        if not isinstance(genre, str) or not genre:
+            raise ValueError("Genre must be a non-empty string")
+        
+        self._name = name
+        self._artist = artist
+        self._genre = genre
+        
+        # Update class attributes
+        Song.count += 1
+        Song.all.append(self)
+        
+        # Track unique genres
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        
+        # Track unique artists
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+        
+        # Update genre count
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        
+        # Update artist count
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def artist(self):
+        return self._artist
+
+    @property
+    def genre(self):
+        return self._genre
+
+    @classmethod
+    def get_all(cls):
+        return cls.all
+
+    @classmethod
+    def get_count(cls):
+        return cls.count
